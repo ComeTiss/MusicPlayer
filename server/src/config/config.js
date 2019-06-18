011320815a06ca39fae5698c2784742bf6ccca2e
ComeTiss <cometisserand@rocketmail.com>
2019-06-18 08:34:00 +0100

    config file added to server

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -12,7 +12,7 @@ module.exports = {
  port: process.env.PORT || 8081,
  db: {
    database: process.env.DB_NAME || 'blacksmith',
-   user: process.env.DB_USER || 'root',
+   user: process.env.DB_USER || '',
    password: process.env.DB_PASS || '',
    options: {
      dialect: process.env.DIALECT || 'sqlite',
